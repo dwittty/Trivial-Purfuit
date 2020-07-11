c58feae99909e03fd631a295393f207e5c759b6f
dwittty <66267250+dwittty@users.noreply.github.com>
2020-07-11 12:53:21 -0400

    Added a button to roll dice
    Added a button that rolls the dice and prints out the result in the console.

---
+++ Trivial Purfuit Game/Assets/C# Scripts/CountClicks.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CountClicks : MonoBehaviour
+{
+    int n;
+    public void OnButtonPress()
+    {
+        n++;
+        Debug.Log("Button clicked " + n + " times.");
+    }
+}
---
+++ Trivial Purfuit Game/Assets/C# Scripts/RollDice.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RollDice : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    public void OnButtonPress()
+    {
+        int randomInt = Random.Range(1, 7); //Return number 1 to 6 (the top of the range is exclusive, not inclusive)        
+        Debug.Log($"You rolled a {randomInt}.");        
+    }
+}
