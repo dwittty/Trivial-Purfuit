6f97cb67401a14350c4413e4ab25748a60d48864
yjryu83 <66267075+yjryu83@users.noreply.github.com>
2020-07-12 00:29:28 -0400

    Update QuestionDatabase.cs

--- Trivial Purfuit Game/Assets/C# Scripts/QuestionDatabase.cs
+++ Trivial Purfuit Game/Assets/C# Scripts/QuestionDatabase.cs
@@ -9,7 +9,15 @@ public class QuestionDatabase : MonoBehaviour
     private string[] _multipleChoice;
     private int _multipleChoiceSize = 4;
     private int _answer;
-     
+    
+    private string _databaseFile = "QandA.csv";
+
+    public QuestionDatabase()       //constructor
+    {
+        loadFile();
+        updateQuestionSet();
+    }
+
 
     void Start()
     {
@@ -20,5 +28,41 @@ public class QuestionDatabase : MonoBehaviour
     void Update()
     {
         
+    }
+    
+    //public functions - will be called by Rule Controller
+ 
+    public string getQuestion()
+    {
+        return _question;
+    }
+
+    public string getMultipleChoide()
+    {
+
+        return _multipleChoice;
+    }
+
+    public string getAnswer()
+    {
+
+        return _answer;
+    }
+
+
+    public void updateQuestionSet()
+    {
+        //this will update the Question, answer and multiple choices.
+        //to be implemented
+    }
+
+    /// <summary>
+    /// /////////////private function ////////////////////////////
+    /// </summary>
+
+    private void loadFile()
+    {
+        //this will load database file
+        //to be implemented
     }
 }
