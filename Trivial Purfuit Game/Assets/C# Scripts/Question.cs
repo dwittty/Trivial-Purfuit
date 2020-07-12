523f3ca0dadf00204bf0635fa81a260dd37b6da6
dwittty <66267250+dwittty@users.noreply.github.com>
2020-07-12 16:28:06 -0400

    Updated classes related to Questions
    Added a Question class with QuestingString, CorrectAnswer,  and AnswerChoices properties.  Added List of Questions to the QuestionDatabase (one for each color). Started stubbing out some methods for parsing the CSV file.

---
+++ Trivial Purfuit Game/Assets/C# Scripts/Question.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Question : MonoBehaviour
+{        
+    public string QuestionString { get; set; }
+    public string CorrectAnswer { get; set; }
+    public List<string> AnswerChoices { get; set; }
+    
+
+    public Question(string question, string answer, List<string> choices)
+    {
+        QuestionString = question;
+        CorrectAnswer = answer;
+        AnswerChoices = choices;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Trivial Purfuit Game/Assets/C# Scripts/QuestionDatabase.cs
+++ Trivial Purfuit Game/Assets/C# Scripts/QuestionDatabase.cs
@@ -1,15 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEditor;
+using System.IO;
+using System.Linq;
 
 public class QuestionDatabase : MonoBehaviour
 {
     // Start is called before the first frame update
+    public List<Question> BlueQuestions { get; set; }
+    public List<Question> RedQuestions { get; set; }
+    public List<Question> GreenQuestions { get; set; }
+    public List<Question> WhiteQuestions { get; set; }
+
+    private List<Question> DiscardPile_BlueQuestions { get; set; }
+    private List<Question> DiscardPile_RedQuestions { get; set; }
+    private List<Question> DiscardPile_GreenQuestions { get; set; }
+    private List<Question> DiscardPile_WhiteQuestions { get; set; }
+
     private string _question;
     private string[] _multipleChoice;
     private int _multipleChoiceSize = 4;
     private int _answer;
-    
+
+    string databasePath = "Assets/TextFiles/test.csv";
+
     private string _databaseFile = "QandA.csv";
     
     private int _category;
@@ -68,11 +83,60 @@ public class QuestionDatabase : MonoBehaviour
     /// <summary>
     /// /////////////private function ////////////////////////////
     /// </summary>
-
     private void loadFile()
     {
+
+        using(StreamReader streamReader = new StreamReader(databasePath))
+        {
+            var questionInfo = new List<string>();
+            string currentLine;
+            while((currentLine = streamReader.ReadLine()) != null)
+            {
+                questionInfo = currentLine.Split(',')
+                                   .ToList();
+                ParseQuestionFromCSVFile(questionInfo);
+            }
+        }
         //this will load database file
         //to be implemented
+    }
+
+    //Breaks out the list from the CSV into its constituent parts, then makes call to add to QuestionDatabase
+    private void ParseQuestionFromCSVFile(List<string> questionInfo)
+    {
+        string color = questionInfo[0];
+        string question = questionInfo[1];
+        string correctAnswer = questionInfo[2];
+        List<string> answerChoices = new List<string> { questionInfo[2], questionInfo[3], questionInfo[4], questionInfo[5] };
+        var newQuestion = new Question(question, correctAnswer, answerChoices);
+        AddQuestionToQuestionDatabase(color, newQuestion);
+    }
+
+    //Determines which color database the question belongs to
+    private void AddQuestionToQuestionDatabase(string color, Question newQuestion)
+    {
+        color = color.ToUpper();
+        switch (color)
+        {
+            case "RED":
+                RedQuestions.Add(newQuestion); break;
+            case "BLUE":
+                BlueQuestions.Add(newQuestion); break;
+            case "GREEN":
+                GreenQuestions.Add(newQuestion); break;
+            case "WHITE":
+                WhiteQuestions.Add(newQuestion); break;
+            default:
+                Debug.Log("This color is unknown."); break;
+
+        }
+    }
+
+    //takes in a list of questions (redlist, bluelist etc) and shuffles the order of those questions 
+    //so that the questions are delivered in a different order every game
+    private void ShuffleQuestions(List<string> questions)
+    {
+
     }
     
 
