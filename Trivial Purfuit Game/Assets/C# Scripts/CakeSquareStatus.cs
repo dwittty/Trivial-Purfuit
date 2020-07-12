bb9edd86efbb7eb373f5b76bd96ce84db4741ad2
yjryu83 <66267075+yjryu83@users.noreply.github.com>
2020-07-12 00:01:27 -0400

    Update CakeSquareStatus.cs

--- Trivial Purfuit Game/Assets/C# Scripts/CakeSquareStatus.cs
+++ Trivial Purfuit Game/Assets/C# Scripts/CakeSquareStatus.cs
@@ -6,9 +6,11 @@ public class CakeSquareStatus : MonoBehaviour
 {
     private bool[,] _isFilled;
     private int _numColor = 4;
+    private int _numUsers;
+    
     public CakeSquareStatus(int numUsers)
     {
-
+        _numUsers = numUsers;
         _isFilled = new bool[numUsers ,_numColor];
         for (int i=0;i<numUsers;i++)
         {
@@ -30,5 +32,34 @@ public class CakeSquareStatus : MonoBehaviour
     void Update()
     {
         
+    }
+    private void setStatus(int player,int color,bool update)        //setter
+    {
+        _isFilled[player][color] = update;
+    }
+
+    private int getStatus(int player, int color)                    //getter
+    {
+        return _isFilled[player][color];
+    }
+
+    public int provideStatus(int player,int color)
+    {
+        return getStatus(player,color);
+    }
+
+    public bool isFull(int player)
+    {
+        bool temp = true;
+       for (int i=0;i<_numColor;i++)
+        {
+
+            if (_isFilled[player][i]==false)
+            {
+                temp = false;
+            }
+
+        }
+        return temp;
     }
 }
