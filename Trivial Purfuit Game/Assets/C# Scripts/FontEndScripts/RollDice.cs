5cf89f3a8cfff4cfc5c4e049255471449090f92e
dwittty <66267250+dwittty@users.noreply.github.com>
2020-07-14 00:52:51 -0400

    Dice roll result displays in UI as text
    Dice roll result displays in UI as text

--- Trivial Purfuit Game/Assets/C# Scripts/FontEndScripts/RollDice.cs
+++ Trivial Purfuit Game/Assets/C# Scripts/FontEndScripts/RollDice.cs
@@ -1,25 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class RollDice : MonoBehaviour
+public class RollDice: MonoBehaviour
 {
+    public Text textField;
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 
-
-    public void OnButtonPress()
+    public void SetText()
     {
         int randomInt = Random.Range(1, 7); //Return number 1 to 6 (the top of the range is exclusive, not inclusive)        
-        Debug.Log($"You rolled a {randomInt}.");        
+        Debug.Log($"You rolled a {randomInt}.");
+        textField.text = "The dice result is: " + randomInt.ToString();
     }
+
 }
