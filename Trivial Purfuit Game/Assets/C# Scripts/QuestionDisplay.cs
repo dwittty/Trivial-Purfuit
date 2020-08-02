3fb2e8d9f09c87b92575f732201dd52daaf71c79
yjryu83 <66267075+yjryu83@users.noreply.github.com>
2020-08-01 23:37:48 -0400

    Answer randomization included

--- Trivial Purfuit Game/Assets/C# Scripts/QuestionDisplay.cs
+++ Trivial Purfuit Game/Assets/C# Scripts/QuestionDisplay.cs
@@ -6,6 +6,7 @@ using UnityEngine.UI;
 
 public class QuestionDisplay : MonoBehaviour
 {
+    private static System.Random random = new System.Random();
     // Start is called before the first frame update
     void Start()
     {
@@ -44,13 +45,52 @@ public class QuestionDisplay : MonoBehaviour
     {
         var questionText = FindObjectsOfType<Text>().FirstOrDefault(x => x.name == "Question");
         questionText.text = question.Prompt;
+
+        var answers = new Button[4];
+        var shuffled = new string[4];
+        shuffled[0] = question.Correct;
+        shuffled[1] = question.Wrong1;
+        shuffled[2] = question.Wrong2;
+        shuffled[3] = question.Wrong3;
+
+        int size = 4;
+        while (size > 1)
+        {
+            size--;
+            int k = random.Next(size + 1);
+            string value = shuffled[k];
+            shuffled[k] = shuffled[size];
+            shuffled[size] = value;
+        }
+
+
+
+
+
+        answers[0]= FindObjectsOfType<Button>().FirstOrDefault(x => x.name == "AnswerA");
+        answers[0].GetComponentInChildren<Text>().text = shuffled[0];
+
+        answers[1] = FindObjectsOfType<Button>().FirstOrDefault(x => x.name == "AnswerB");
+        answers[1].GetComponentInChildren<Text>().text = shuffled[1];
+
+        answers[2] = FindObjectsOfType<Button>().FirstOrDefault(x => x.name == "AnswerC");
+        answers[2].GetComponentInChildren<Text>().text = shuffled[2];
+
+        answers[3] = FindObjectsOfType<Button>().FirstOrDefault(x => x.name == "AnswerD");
+        answers[3].GetComponentInChildren<Text>().text = shuffled[3];
+
+        /*
         var answerA = FindObjectsOfType<Button>().FirstOrDefault(x => x.name == "AnswerA");
         answerA.GetComponentInChildren<Text>().text = question.Correct;
+        
         var answerB = FindObjectsOfType<Button>().FirstOrDefault(x => x.name == "AnswerB");
         answerB.GetComponentInChildren<Text>().text = question.Wrong1;
+        
         var answerC = FindObjectsOfType<Button>().FirstOrDefault(x => x.name == "AnswerC");
         answerC.GetComponentInChildren<Text>().text = question.Wrong2;
+        
         var answerD = FindObjectsOfType<Button>().FirstOrDefault(x => x.name == "AnswerD");
         answerD.GetComponentInChildren<Text>().text = question.Wrong3;
+        */
     }
 }
