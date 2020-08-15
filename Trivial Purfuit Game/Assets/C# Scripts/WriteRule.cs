cad46850ef2662d8d0ab587b203f7b127c3853e1
yjryu83 <66267075+yjryu83@users.noreply.github.com>
2020-08-14 23:08:42 -0400

    Add files via upload

---
+++ Trivial Purfuit Game/Assets/C# Scripts/WriteRule.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System.IO;
+
+public class WriteRule : MonoBehaviour
+{
+
+    private static readonly string ResourcePath = "TextFiles/gameRule";
+    private TextAsset gamerule;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        postRule();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    void postRule()
+    {
+        gamerule = Resources.Load(ResourcePath, typeof(TextAsset)) as TextAsset;
+        //string path = "Assets/Resources/TextFiles/Trivial Purfuit Game Rules.docx";
+        
+        //fgCSVReader.LoadFromTextAsset(questionFile, new fgCSVReader.ReadLineDelegate(ReadLineIntoQuestion));
+
+        //StreamReader reader = new StreamReader(path);
+        var guiTextObject = this.gameObject.GetComponent<Text>();
+        
+        guiTextObject.text = $"{gamerule.ToString()}";
+        
+        //reader.Close();
+    }
+
+    
+
+        
+    
+}
